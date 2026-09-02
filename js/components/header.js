777b61395f1e2513a675719a17b80a178809cc12
SophiaHesteng <sophes01@iba.dk>
2026-09-02 13:37:48 +0200

    kortet er færdig!

--- js/components/header.js
+++ js/components/header.js
@@ -21,13 +21,41 @@ export function renderHeader() {
             <div class="map-menu" hidden>
 
             <nav class="map-nav" aria-label="Hovednavigation">
-                <a href="forside.html" class="map-place map-place-front" data-page="forside.html">Forsiden</a>
-                <a href="about.html" class="map-place map-place-about" data-page="about.html">Om os</a>
-                <a href="contact.html" class="map-place map-place-contact" data-page="contact.html">Kontakt</a>
-                <a href="receptionen.html" class="map-place map-place-reception" data-page="receptionen.html">Receptionen</a>
-                <a href="faq.html" class="map-place map-place-questions" data-page="faq.html">Spørgehjørnet</a>
-                <a href="proeverummet.html" class="map-place map-place-proeverum" data-page="proeverummet.html">Prøverummet</a>
-                <a href="library.html" class="map-place map-place-library" data-page="library.html">Biblioteket</a>
+
+                <svg class="map-route" viewBox="0 0 425 250" aria-hidden="true">
+                    <path d="M 110 45
+                        C 125 75, 145 85, 160 65
+                        C 180 40, 205 55, 220 60
+                        C 245 65, 270 55, 280 60" />
+
+                    <path d="M 310 68
+                        C 310 75, 315 78, 320 85" />
+
+                    <path d="M 90 45
+                        C 115 72, 115 88, 110 98" />
+
+                    <path d="M 80 110
+                        C 70 115, 85 125, 75 130
+                        C 65 138, 55 148, 45 152" />
+
+                    <path d="M 130 122
+                        C 140 130, 150 140, 160 145
+                        C 170 150, 165 155, 168 158" />
+
+                    <path d="M 180 110
+                        C 195 112, 205 120, 220 125
+                        C 235 130, 245 125, 255 135
+                        C 265 145, 265 165, 270 175
+                        C 275 182, 270 188, 265 192" />
+                </svg>
+
+                    <a href="forside.html" class="map-place map-place-front" data-page="forside.html">Forsiden</a>
+                    <a href="about.html" class="map-place map-place-about" data-page="about.html">Om os</a>
+                    <a href="contact.html" class="map-place map-place-contact" data-page="contact.html">Kontakt</a>
+                    <a href="receptionen.html" class="map-place map-place-reception" data-page="receptionen.html">Receptionen</a>
+                    <a href="faq.html" class="map-place map-place-questions" data-page="faq.html">Spørgehjørnet</a>
+                    <a href="proeverummet.html" class="map-place map-place-proeverum" data-page="proeverummet.html">Prøverummet</a>
+                    <a href="library.html" class="map-place map-place-library" data-page="library.html">Biblioteket</a>
             </nav>
             </div>
         </div>
